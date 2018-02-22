e7b15f874fdde8dd194787e21410c17c9074ce45
Jesse Hatton <odloads@live.com>
2018-02-22 07:44:39 -0500

    not much accomplished

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,7 +1,8 @@
 class CartController < ApplicationController
 
   before_action :authenticate_user!, except: [:add_to_cart, :view_order]
-
+  
+  
   def add_to_cart
     line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
 
@@ -14,11 +15,21 @@ class CartController < ApplicationController
     @line_items = LineItem.all
   end
 
+  def count_cart
+    @count = 0
+
+    LineItem.all.each do |item|
+     if (item.cart_id == @cart.id)
+       @count += item.quantity
+     end
+    end
+  end
+
   def checkout
-    line_items = LineItem.all
+    @line_items = LineItem.all
     @order = Order.create(user_id: current_user.id, subtotal: 0)
 
-    line_items.each do |line_item|
+    @line_items.each do |line_item|
       line_item.product.update(quantity: (line_item.product.quantity - line_item.quantity))
       @order.order_item[line_item.product_id] = line_item.quantity 
       @order.subtotal += line_item.line_item_total
@@ -28,6 +39,6 @@ class CartController < ApplicationController
     @order.update(sales_tax: (@order.subtotal * 0.08))
     @order.update(grand_total: (@order.sales_tax + @order.subtotal))
 
-    line_items.destroy_all
+    @line_items.destroy_all
   end
 end
